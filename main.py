e1e51e1ae623c4229fe5bece03f58bb4f259265a
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-07-18 17:44:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from tkinter import *
 from tkinter import filedialog
 import tkinter as tk
 
-def select_pasta():
+def select_path():
+  global path
   path = filedialog.askdirectory()
 
-# def apagar_2x():
-#   for arquivo in os.walk(path):
-#     if "@2x" in arquivo:
-#       print(arquivo)
-#       os.remove(f'{path}/{arquivo}')
+def apagar_2x():
+  global path
+  for arquivo in os.listdir(path):
+    if "@2x" in arquivo:
+      print(arquivo)
+      os.remove(f'{path}/{arquivo}')
 
 name = []
 root = Tk()
@@ -21,7 +23,16 @@ root.resizable(height=False, width=False)
 
 button_path = tk.Button(root,
 text='Select your folder',
-command=select_pasta
+command=select_path,
+font=("Arial Bold", 12),
 )
-button_path.grid(padx=50,pady=100)
+
+button_apagar = tk.Button(root,
+text='Delete',
+command=apagar_2x,
+font=("Arial Bold", 12),
+)
+
+button_path.pack(side = LEFT)
+button_apagar.pack(side = RIGHT)
 root.mainloop()
