f787e1ff14705d7255255df95a6724931abf9382
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-07-30 17:07:46 -0400

    update

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ from tkinter import filedialog
 import time
 
 while True:
+  arquivos = []
   path = filedialog.askdirectory()
-
-  print(f'- Diretório Selecionado: {os.path.dirname(path)}')
-  print('Os seguintes arquivos serão excluídos: ')
 
   for arquivo in os.listdir(path):
     if "@2x" in arquivo:
-      print(arquivo)
+      arquivos.append(arquivo)
+  
+  if len(arquivos) == 0:
+    print("- Nenhum arquivos @2x encotrado")
+    continue
+  else:
+    print('A seguinte quantidade de arquivos serão excluídos: ', len(arquivos))
 
-  choice = input("Deixa mesmo continuar? (S/N) ").capitalize()
+    choice = input("Deixa mesmo continuar? (S/N) ").capitalize()
 
-  if choice not in ['N','S']:
-    print('Escolha inválida')
+    if choice not in ['N','S']:
+      print('Escolha inválida')
+      continue
 
-  elif choice == 'S':
-    for arquivo in os.listdir(path):
-      if "@2x" in arquivo:
-        os.remove(f'{path}/{arquivo}')
+    elif choice == 'S':
+      for arquivo in os.listdir(path):
+        if "@2x" in arquivo:
+          os.remove(f'{path}/{arquivo}')
+      print("Encerrando")
+      break
 
-  elif choice == 'N':
-    print('Cancelando...')
-    time.sleep(2)
-    break
+    elif choice == 'N':
+      print('Cancelando...')
+      time.sleep(2)
+      break
