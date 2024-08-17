f0d7877887c57e43a7a73b8446b421c0314a0450
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-08-17 12:12:29 -0400

    finishing gui

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from tkinter import filedialog, ttk
+from tkinter import filedialog, ttk,messagebox
 from tkinter import *
 
 arquivos = []
@@ -13,6 +13,7 @@ def delete():
   for arquivo in os.listdir(path):
     if "@2x" in arquivo:
       os.remove(f'{path}/{arquivo}')
+  messagebox.showinfo("Popup", "All @2x deleted!") 
 
 def menu():
   for arquivo in os.listdir(path):
@@ -20,21 +21,12 @@ def menu():
       arquivos.append(arquivo)
 
   if len(arquivos) == 0:
-    top= Toplevel(root)
-    top.geometry("200x100")
-    top.resizable(False, False)
-    top.title("Popup")
-
-    Label(top, text= "@2x Not Found", font=("Arial", 12, "bold")).place(relx=0.5, rely=0.4, anchor=CENTER)
-    Button(top, text= "Close", command=top.destroy, font=("Arial", 12)).place(relx=0.5, rely=0.7, anchor=CENTER)
+    messagebox.showinfo("Popup", "No @2x found") 
 
   else:
-    top= Toplevel(root)
-    top.geometry("400x200")
-    top.resizable(False, False)
-    Label(top, text= f" {len(arquivos)} arquivos serão excluídos, tem certeza?", font=("Arial", 10, "bold")).place(relx=0.5, rely=0.4, anchor=CENTER)
-    Button(top, text= "Sim", command=top.destroy, font=("Arial", 12)).place(relx=0.4, rely=0.7, anchor=CENTER)
-    Button(top, text= "Cancelar", command=top.destroy, font=("Arial", 12)).place(relx=0.6, rely=0.7, anchor=CENTER)
+    mensagem = messagebox.askquestion("askquestion", f"{len(arquivos)} files will be erased, are you sure?")
+    if mensagem:
+      delete()
 
 root = Tk()
 
@@ -52,4 +44,3 @@ btn2 = Button(root, text="Delete @2x", command=menu, height=2, width=20)
 btn2.place(relx=0.5, rely=0.75, anchor=CENTER)
 
 root.mainloop()
-
